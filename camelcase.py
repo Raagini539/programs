9f643cb54b072e978075619e5addf2393c14ead7
Raagini539 <46399940+Raagini539@users.noreply.github.com>
2019-02-01 00:25:44 +0530

    Create camelcase.py

---
+++ camelcase.py
@@ -0,0 +1,9 @@
+str=input()
+str=lower(str)
+words=input.split(' ')
+capitalized=[]
+for word in words:
+  final_words=word[0].upper()+word[1:]
+  capitalized.append(final_words)
+output=' '.join(capitalized)
+print(output)
