08df0c64f85882761d04db16400bbaefdeea9646
Raagini539 <46399940+Raagini539@users.noreply.github.com>
2019-02-01 13:04:12 +0530

    Update diffbyonech.py

--- diffbyonech.py
+++ diffbyonech.py
@@ -1,7 +1,9 @@
-#ragini
+#raagini
 count=0
-str1=input()
-str2=input()
+strww=input().split()
+str1=strww[0]
+str2=strww[1]
+
 for i in range(0,len(str1)):
   for j in range(0,len(str2)):
     if(str1[i]!=str2[j]):
@@ -9,4 +11,4 @@ for i in range(0,len(str1)):
 if(count==1):
   print("yes")
 else:
-  print("no")
+	print("no")
