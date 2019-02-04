4ea6a8549013761059358838af88297a0fa8b9dc
Raagini539 <46399940+Raagini539@users.noreply.github.com>
2019-02-04 13:58:38 +0530

    Update fivemultiples.py

--- fivemultiples.py
+++ fivemultiples.py
@@ -1,14 +1,5 @@
-#include<stdio.h>
-int main()
-{
-int n,i;
-printf("enter the number",n)
-for(i=1;i<n;i++)
-{
-if(i%5==0)
-{
-printf("%d\n",i);
-}
-}
-return 0;
-}
+prod=1
+n=input()
+for i in range(1,6):
+  prod=n*i
+  print(prod," ")
