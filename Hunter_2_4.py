02704241bab17046b55642db68ec6b0b7def3904
Raagini539 <46399940+Raagini539@users.noreply.github.com>
2019-03-24 19:06:12 +0530

    Create Hunter_2_4.py

---
+++ Hunter_2_4.py
@@ -0,0 +1,10 @@
+#raagini
+fromitertools import permutations
+
+in_str = input()
+
+out_list = permutations(in_str)
+out_list = list(dict.fromkeys(out_list))
+
+for perm in out_list:
+    print(''.join(perm))
