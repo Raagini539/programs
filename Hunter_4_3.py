e4a8cb4a691bfffd1cbe88c9c3fc0fd861a1e3ee
Raagini539 <46399940+Raagini539@users.noreply.github.com>
2019-03-26 18:07:04 +0530

    Create Hunter_4_3.py

---
+++ Hunter_4_3.py
@@ -0,0 +1,18 @@
+#raagini
+a=input()
+s=''
+m=0
+i=0
+while i<len(a)-1:
+  if a[i]=='a' and a[i+1]=='b':
+    s+=a[i]+a[i+1]
+    i+=2
+  else:
+    s=''
+    i+=1
+  if m<len(s):
+    m=len(s)
+b=len(a)-1
+if a[b]=='a' and a[b-1]=='b' and s!='':
+  m+=1
+print(m)
