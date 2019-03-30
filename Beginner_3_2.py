b4d458b0e3c94a04a5d95026343ac4234105a49a
Raagini539 <46399940+Raagini539@users.noreply.github.com>
2019-03-30 19:57:48 +0530

    Create Beginner_3_2.py

---
+++ Beginner_3_2.py
@@ -0,0 +1,9 @@
+//raagini
+maxele.py
+
+lst=[]
+num = int(input("enter the number: "))
+for n in range(num):
+    numbers = int(input("Enter number:"))
+    lst.append(numbers)
+print("Maximum element in the list is :", max(lst)
